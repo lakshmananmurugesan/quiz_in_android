80451907c18045b2c252f4b85585dda2028c15cd
Lakshman <ameex@ameex.(none)>
2016-01-15 13:12:01 +0530

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Quizapp::Application.routes.draw do
+
+  devise_for :users
+
   resources :questions
 
   get "quiz/index"
@@ -66,7 +69,7 @@ Quizapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+
 
   # See how all your routes lay out with "rake routes"
 
